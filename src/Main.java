552d5062e28dd68522806c4a96c6a88f68c30bda
Alina <77508351+Alina1103master@users.noreply.github.com>
2021-04-08 07:45:51 +0300

    change file

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int starting = 100;
         int bonus = 1;
         int payment = 3500;
-        if (payment >= 1000) bonus = payment/100;
+        if (payment <= 1000) bonus = payment/100;
         int balance = payment + starting + bonus;
         System.out.println(bonus);
 
